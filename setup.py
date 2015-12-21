2affa2a34df3a0b5b323513a5d53e9bd0ce0be0e
generale <robbyetor@gmail.com>
2015-12-21 20:50:49 +0100

    setup.py and distribution utils

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='trackClosure',
+      version='0.1',
+      description='',
+      url='https://github.com/micheleAlberto/trackClosure',
+      author='Michele Alberto',
+      author_email='robbyetor@gmail.com',
+      packages=['trackClosure'],
+      zip_safe=False)
