430f5d51b2164079392459b887a40f77f5dcac3e
generale <robbyetor@gmail.com>
2015-12-29 03:33:16 +0100

    minor

--- trackClosure/apps/trackVisualizer.py
+++ trackClosure/apps/trackVisualizer.py
@@ -16,21 +16,16 @@ from ..src.fromOpenMVG.wrapper import OpenMVG
 from ..src.trackvis import trackvis as vis
 #find getIMG
 
-        right_key=1113939%256
-        left_key=1113937%256
-        up_key=1113938%256
-        down_key=1113940%256
-        new_key=1048686%256 #n
-        del_key=1048676%256 #d
-        toggle_label=1048684%256 #l
-        toggle_epilines=1048677%256 #e
-        quit_key= 1048689%256 #q
+right_key=1113939%256
+left_key=1113937%256
+quit_key= 1048689%256 #q
 
 def visualize(
         image_dir,
         cc_track_file,
         epipolar_geometry_file,
-        cc_ids):
+        cc_ids,
+        scale):
     omvg=OpenMVG()
     omvg.set_image_dir(image_dir)
     omvg.set_feature_dir("data")
@@ -40,7 +35,7 @@ def visualize(
     print "connected components file: ",cc_track_file, " id:",cc_ids
     for cc_id in cc_ids:
         cc=CC.points[cc_id]
-        visualize_cc(cc, gEpg, image_id2name)
+        visualize_cc(cc, gEpg, image_id2name,scale)
 
 def shift_left(l):
     return l[1:]+[l[0]]
@@ -49,8 +44,8 @@ def shift_right(l):
     return [l[-1]]+l[:-1]
 
 def draw_kps(IM, V,scale):
-     #draw all keypoints with color ORACLE2COLOR[VIEW2ORACLE[VIEW]]
-     for v in V:
+    #draw all keypoints with color ORACLE2COLOR[VIEW2ORACLE[VIEW]]
+    for v in V:
         v_id = (v.id_image,v.id_keypoint)
         color = vis.BLUE
         vis.drawView(IM,v,color=color,img_scale=scale)
@@ -59,8 +54,8 @@ def draw_kps(IM, V,scale):
 def draw_epiline(IM, im, cc, gEpG,scale):
     epipolarGeometries=vis.filterEpG(gEpG, cc.views)
     for im_id in cc.views:
-        if (im_id is not im) 
-        and (im_id,im) in epipolarGeometries):
+        if ((im_id is not im) 
+        and ((im_id,im) in epipolarGeometries)):
             F=epipolarGeometries[(im_id,im)]
             for v in cc.views[im_id]:
                 v_id=v.key()
@@ -82,14 +77,14 @@ def visualize_cc(cc, gEpg, image_id2name,scale):
         if redraw:
             images = [I[i] for i in image_indexes[:3]]
             redraw=False
-            GAL=imgallery(images,show=False)
+            GAL = imgallery(images,show=False)
             cv2.imshow(str(cc.id),GAL)
-        key=cv2.waitKey(20)%256
+        key = cv2.waitKey(20)%256
         if key==right_key:
-            image_indexes=shift_right(image_indexes)
-            redraw=True
-        elif key=left_key:
-            image_indexes=shift_left(image_indexes)
+            image_indexes = shift_right(image_indexes)
+            redraw = True
+        elif key==left_key:
+            image_indexes = shift_left(image_indexes)
             redraw=True
 
 def main(argv):
@@ -105,7 +100,8 @@ def main(argv):
         argv[1], # image_dir,
         argv[2], # cc_track_file,
         argv[3], # epipolar_geometry_file,
-        argv[4:]) # cc_ids)
+        argv[4:], # cc_ids
+            1.)  # scale
 
 if __name__ == "__main__":
     main(sys.argv)
